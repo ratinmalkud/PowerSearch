6ee4568d914fd3b2ba30a0f01d166e4af060e065
Ameya <Ameya@Ameya-PC>
2013-04-04 03:43:58 -0400

    Basic Home Screen+Text Search.
    TODO: 1>Fix for different resolutions
    	  2>Make display good-looking
    	  3>Decide on naming convention

--- PowerSearch/src/com/powerSearch/HomeScreen.java
+++ PowerSearch/src/com/powerSearch/HomeScreen.java
@@ -1,18 +1,46 @@
 package com.powerSearch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeScreen extends Activity {
 
+	Button location_search;
+	Button image_search;
+	Button text_search;
+	Button audio_search;
+	
+	String text_input;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
 		
+		location_search = (Button) findViewById(R.id.button_location_search);
+		image_search = (Button) findViewById(R.id.button_image_search);
+		audio_search = (Button) findViewById(R.id.button_audio_search);
+		text_search = (Button) findViewById(R.id.button_text_search);
+
+		text_search.setOnClickListener(new OnClickListener(){
+			public void onClick(View V){
+			System.out.println("IN onclick");
+			start_text_search();
+			}
+	});
 	}
 
+	private void start_text_search(){
+		Intent i = new Intent(this, Text_screen1.class);
+		startActivity(i);
+		finish();
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
