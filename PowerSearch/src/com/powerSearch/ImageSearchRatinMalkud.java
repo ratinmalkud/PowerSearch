841735b09b3e7b770ae44bcba8abad38b6736db2
Ratin Malkud <ratinmalkud@cmu.edu>
2013-04-26 10:58:36 -0400

    Cleaned image search.

--- PowerSearch/src/com/powerSearch/ImageSearchRatinMalkud.java
+++ PowerSearch/src/com/powerSearch/ImageSearchRatinMalkud.java
@@ -25,8 +25,10 @@ import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Media;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -55,6 +57,7 @@ public class ImageSearchRatinMalkud extends Activity {
 	int width;
 	int height;
 	int bpp = 12;
+	String result;
 		
 	/*********************************************************************************************/
 	/**
@@ -104,24 +107,11 @@ public class ImageSearchRatinMalkud extends Activity {
 
 	private Handler handler;
 
-//	private Preview preview;
-
-	private ImageButton snapButton;
-
-	private ImageButton btnShowList;
-
-	private ImageButton tutoButton;
-
 	static IQE iqe;
-
-	private AlertDialog ad;
 	
-	private QueryProgressDialog pd;
+	private String lastPostedQid = null;
 
 	private AtomicBoolean activityRunning = new AtomicBoolean(false);
-	
-//	private Preview.FrameReceiver mreceiver;
-	/********************************************************************************************/
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -150,13 +140,8 @@ public class ImageSearchRatinMalkud extends Activity {
 		return true;
 	}
 	
-	public void takePicture(View view){
-		
-	}
-	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if(requestCode==0 && resultCode==Activity.RESULT_OK){
-			// Now we know that our pic URI refers to the image just taken
 			try {
 				bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), pic);
 				startSearch();
@@ -173,12 +158,10 @@ public class ImageSearchRatinMalkud extends Activity {
 			startActivity(intent);
 		}
 	}
-
 		
 	public void startSearch(){
 		width = bmp.getWidth();
 		height = bmp.getHeight();
-		//frames = new byte[((width * height * bpp) / 8)];
 		
 		int [] argb = new int[width * height];
 	    bmp.getPixels(argb, 0, width, 0, 0, width, height);
@@ -188,13 +171,8 @@ public class ImageSearchRatinMalkud extends Activity {
         
         yuvs = new YuvImage(yuv, ImageFormat.NV21, width, height, null);
 		
-		iqe.sendMessageAtFrontOfQueue(iqe.obtainMessage(IQE.CMD_DECODE, IQE.snap, 0, yuv));
-	//	processImageSnap(yuv);
+		iqe.sendMessageAtFrontOfQueue(iqe.obtainMessage(IQE.CMD_DECODE, IQE.snap, 0, yuvs));
 	}
-		//yuv = new YuvImage(array, ImageFormat.NV21, width, height, null); 
-		//iqe.sendMessageAtFrontOfQueue(iqe.obtainMessage(IQE.CMD_DECODE, IQE.snap, 0, yuvs));
-//			processImageSnap(yuv);
-	//	}
 		
 	byte[] encodeYUV420SP(byte[] yuv420sp, int[] argb, int width, int height) {
         final int frameSize = width * height;
@@ -232,32 +210,8 @@ public class ImageSearchRatinMalkud extends Activity {
         return yuv420sp;
     }
 	
-	/***********************************************************************************************/
 	private Runnable postponedToastAction;
-	private void processImageSnap(final YuvImage yuv) {
-
-	postponedToastAction = new Runnable() {
-			public void run() {
-				if (SEARCH_OBJECT_REMOTE) {
-				Toast.makeText(
-						getBaseContext(),
-						"This may take a minute... We will notify you when your photo is recognized.",
-						Toast.LENGTH_LONG).show();
-				}
-			//	//pd.dismiss();
-				//pd.pdDismissed();
-			//	unfreezePreview();
-			//	iqe.goScan();
-			//	snapButton
-			//	.setImageResource(R.drawable.ic_camera);
-		}
-	};
-		handler.postDelayed(postponedToastAction, REMOTE_MATCH_MAX_DURATION);
-
-		if (DEBUG) Log.d(TAG," snap decode message");
-	iqe.sendMessageAtFrontOfQueue(iqe.obtainMessage(IQE.CMD_DECODE, IQE.snap, 0, yuv));
-}
-	
+		
 	private OnResultCallback onResultCallback = new OnResultCallback() {
 
 		/**
@@ -275,31 +229,7 @@ public class ImageSearchRatinMalkud extends Activity {
 
 		@Override
 		public void onQueryIdAssigned(String queryId, String path, int callType) {
-
-			switch (callType) {
-			
-			case (IQE.scan):
-			//	createHistoryItem(queryId, path, IQE.scan);
-				break;
-			
-			case (IQE.snap):
-				createHistoryItem(queryId, path, IQE.snap);
-				lastPostedQid = queryId;
-				Log.d(TAG, "before pd");
-		//		iqe.goScan();
-		//		Message.obtain(iqe, IQE.CMD_DECODE, IQE.scan, 0, yuv).sendToTarget();
-			/*	if (SEARCH_OBJECT_REMOTE) {
-					handler.post(new Runnable() {
-						@Override
-						public void run() {
-							
-							pd.setMessage("Searching...");
-							Log.d(TAG, "after pd");
-						}
-					});
-				} */
-				break;
-			}
+			lastPostedQid = queryId;
 		}
 
 		/**
@@ -320,76 +250,16 @@ public class ImageSearchRatinMalkud extends Activity {
 		@Override
 		public void onResult(String queryId, String objId, String objName,
 				String objMeta, int engine, final int callType) {
-			Log.d("--------------------------", "IN onResult");
+			
 			final String qId = queryId;
 			final String oNm = objName;
-			Log.d("----------------","in on result");
-			// if the it is a barcode
-			if (engine == IQE.barcode) {
-				handler.post(new Runnable() {
-					public void run() {
-						if (callType==IQE.snap) {
-							handler.removeCallbacks(postponedToastAction);
-						}
-						processSearchResult(qId, oNm, null, IQE.scan);
-					}
-				});
-				return;
-			}
+			result = oNm;
 			
-			//if it is a local match
-			if (engine == IQE.local){
-				handler.post(new Runnable() {
-					public void run() {
-						if (callType==IQE.snap) {
-							handler.removeCallbacks(postponedToastAction);
-						}
-						processSearchResult(qId, oNm, null, IQE.scan);
-					}
-				});
-				return;
-			}
-			
-			//if it is a remote match
-			else {
-			//	if (queryId.equals(lastPostedQid)) {
-				//	handler.removeCallbacks(postponedToastAction);
-				Toast.makeText(getBaseContext(),oNm,Toast.LENGTH_SHORT).show();
-				//	if (queryId.equals(lastPostedQid)) {
-				//	handler.removeCallbacks(postponedToastAction);
-				}
-			/*	Uri uri = null;
-				// match's Metadata set as URI.
-				if (objMeta != null) {
-
-					try {
-						uri = Uri.parse(objMeta);
-					} catch (Exception e1) {
-						uri = null;
-					}
-				}
-				// if no Metadata : match's name set as URI.
-				if (uri == null) {
-
-					if (objName != null) {
-
-						try {
-							uri = Uri.parse(objName);
-						} catch (Exception e1) {
-							uri = null;
-						}
-					}
-				}
-				final Uri fUri = uri;
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						// process and display the results
-						processSearchResult(qId, oNm, fUri, callType);
-					}
-				});*/
-			}
-	//	}
+			showResult();
+		//	Toast.makeText(getBaseContext(),oNm,Toast.LENGTH_LONG).show();
+		}
+		
+		
 
 		/**
 		 * When no match are found, or exception occurs.
@@ -414,178 +284,32 @@ public class ImageSearchRatinMalkud extends Activity {
 									"Unable to connect to the server. "
 											+ "Check your intenet connection.",
 									Toast.LENGTH_LONG).show();
-						//	pd.dismiss();
-						//	pd.pdDismissed();
-						//	unfreezePreview();
 						}
 					});
 				} else {
 					Log.e(TAG, "Unable to complete search", e);
+					//Have a dialog here that will say search not successful.
 				}
 				return;
 			}
-			// if just nothing found
-			switch (callType) {
-				
-			case (IQE.scan):
-				Log.d("------------","scan now");
-			//	startScanning();
-				break;
-
-			case (IQE.snap):
-				Log.d("-----------------","snap");
-				//displayResult(null, IQE.snap, imgFile);
-				break;
-			}
 		}
 	};
-	
-	private void processSearchResult(String searchId, String label, Uri uri,
-			final int callType) {
-/*
-		HistoryItem item = null;
-	
-		for (Iterator<HistoryItem> iter = history.iterator();;) {
-			if (!iter.hasNext()) {
-				break;
-			}
-			item = iter.next();
-			if (searchId.equals(item.id)) {
-				if (DEBUG) Log.d(TAG, "" + item.id);
-				if (DEBUG) Log.d(TAG, "" + searchId);
-				item.label = label;
-				item.uri = uri;
-				break;
-			} else {
-				item = null;
-			}
-		} 
-
-		if (item == null) {
-			if (DEBUG) Log.w(TAG, "No entry found for qid: " + searchId);
-		//	startScanning();
-			return;
-		}
-		historyListAdapter.notifyDataSetChanged();
-		
-		if (!activityRunning.get()) {
-			return;
-		}
-		*/
-		
-//		does not display the result if the query isn't the last posted
-//		if (!searchId.equals(lastPostedQid)) {
-	//		return;
-	//	}
-		
-		
-		Boolean validUri = false;
-		// Try to display the resources from the Uri. //
-		//********************************************//
-		if (uri == null) {
-			UrlValidator urlValidator = new UrlValidator();
-			if (urlValidator.isValid(label)) {
-				validUri = true;
-				Uri Buri = Uri.parse(label);
-				this.startActivity(new Intent(Intent.ACTION_VIEW, Buri));	
-			}
-
-		} else {
-			validUri = processMetaUri(this, uri);
-		}
-		//*********************************************//
-		/////////////////////////////////////////////////
-
-		// If no Metadata available, just display the match found and the label.
-
-	//	if (!validUri) {
-		//	displayResult(item, callType, null);
-	//	}
-	}
-	
-	static boolean processMetaUri(Activity a, Uri uri) {
-
-		if (uri != null && uri.toString().length() > 0) {
-			try {
-				a.startActivity(new Intent(Intent.ACTION_VIEW, uri));
-				return true;
-			} catch (ActivityNotFoundException e) {
-				Log.w(TAG,
-						"Unable to open view for this meta: " + uri.toString(),
-						e);
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
-	
-	private String lastPostedQid = null;
-	
-	private void createHistoryItem(String qid, String path, int callType) {
-
-		Bitmap thumb = null;
-
-		switch (callType) {
-		/*
-		case (IQE.scan):
-			try {
-				InputStream is = this.getAssets().open(
-						"iqedata/" + path + "/" + path + ".jpg");				
-				Bitmap origBmp = BitmapFactory.decodeStream(is);
-				thumb = transformBitmapToThumb(origBmp);
-			} catch (IOException e) {
-				thumb = null;
-			}
-			break;
-			*/
-		case (IQE.snap):
-			
-		//	Bitmap origBmp = BitmapFactory.decodeFile(path);
-		//	thumb = transformBitmapToThumb(origBmp);
-		//	thumb = Utils.rotateBitmap(thumb, preview.getAngle());
-			
-		}
-
-	//	HistoryItem item = new HistoryItem();
-	//	item.id = lastPostedQid = qid;
-	//	item.label = "Searching...";
-	//	item.uri = null;
-	//	item.thumb = thumb;
-
-	//	if (history.size() > MAX_ITEM_HISTORY)
-	//		history.remove(0);
-
-	//	history.add(item);
-
-		if (DEBUG) Log.d(TAG, "History item created for qid: " + qid);
-	}
 
 	public void onResume() {
 		Log.d(TAG,"in On resume");
 		super.onResume();
 		activityRunning.set(true);
 		iqe.resume();
-	/*	preview.setFrameReceiver(mreceiver);
-		if(preview.mCamera!=null){
-			unfreezePreview();
-		}*/
 	}
 	
-
 	@Override
 	public void onPause() {
 		if (DEBUG) Log.d(TAG, "onPause");
-		
-	//	stopScanning();
 		iqe.pause();
-	//	historyItemDao.saveAll(history);
-	//	preview.stopPreview();
 		activityRunning.set(false);
 		super.onPause();
 	}
 	
-
 	@Override
 	public void onDestroy() {
 		if (DEBUG) Log.d(TAG,"onDestroy");
@@ -593,7 +317,38 @@ public class ImageSearchRatinMalkud extends Activity {
 		super.onDestroy();
 	}
 	
+	public void startAgain(){
+		Intent intent = new Intent(this, ImageSearchRatinMalkud.class);
+		startActivity(intent);
+	}
 	
+	public void showResult(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(ImageSearchRatinMalkud.this);
+		builder.setCancelable(false);
+		builder.setTitle("Image Recognized as...");
+		builder.setMessage(result);
+		builder.setInverseBackgroundForced(true);
+		builder.setPositiveButton("Google it !", new DialogInterface.OnClickListener() {
+		
+			@Override
+		public void onClick(DialogInterface dialog, int id){
+			//perform text search here
+				Intent search = new Intent(Intent.ACTION_WEB_SEARCH);  
+				search.putExtra(SearchManager.QUERY, result);  
+				startActivity(search); 
+		}
+		})
+		.setNegativeButton("Retake image", new DialogInterface.OnClickListener() {
+					
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				startAgain();
+			}
+		});
 	
-	/**********************************************************************************************/
+			
+		AlertDialog myAlert = builder.create();
+		myAlert.show();
+	}
 }
